db29115631e2268f534f5add74ddca7044df9583
OGGY-Dev <ogulcandemirtas@outlook.com>
2021-12-15 22:52:25 +0300

    fibonacci playground hw

---
+++ Fibonacci.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//Ogulcan Demirtas - Week 1 - HW-FIBONACCI-
+
+var fibonacciList = [Int]()
+var n1 = 0
+var n2 = 1
+var result = 0
+
+while result < 1001 {
+    result = n1 + n2
+    n1 = n2
+    n2 = result
+    
+    if result >= 750 && result < 1001 {
+        fibonacciList.append(result)
+    } else if result > 1000{
+        break
+    }
+
+}
+print(fibonacciList)
+
